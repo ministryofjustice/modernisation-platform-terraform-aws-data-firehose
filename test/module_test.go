e4aab1ded2cf5cf9af2ab2f7ca1f0e1c93fc114b
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-03 17:35:04 +0100

    small changes for consistency

--- test/module_test.go
+++ test/module_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestTerraformModule(t *testing.T) {
+func TestModule(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
@@ -22,10 +22,8 @@ func TestTerraformModule(t *testing.T) {
 	assert.Contains(t, cloudwatchLogGroupName, "/aws/kinesisfirehose/cloudwatch-to-s3", "CloudWatch Log Group name should contain '/aws/kinesisfirehose/cloudwatch-to-s3'")
 
 	// Retrieve and test the 'data_stream' output as a map
-	dataStreamMap := terraform.OutputMap(t, terraformOptions, "data_stream")
-    dataStreamName, exists := dataStreamMap["name"]
-    assert.True(t, exists, "'name' key should exist in data_stream output")
-    assert.Contains(t, dataStreamName, "cloudwatch-to-s3", "Data stream name should contain 'cloudwatch-to-s3'")
+	dataStream := terraform.OutputMap(t, terraformOptions, "data_stream")
+    assert.Contains(t, dataStream["name"], "cloudwatch-to-s3", "Data stream name should contain 'cloudwatch-to-s3'")
 
     // Test IAM Roles exist
     iamRoles := terraform.OutputMap(t, terraformOptions, "iam_roles")
@@ -39,7 +37,6 @@ func TestTerraformModule(t *testing.T) {
 	// Test KMS Key ARN and Firehose Server Side Encryption Key ARN match
 	kmsKeyArn := terraform.Output(t, terraformOptions, "kms_key_arn")
 	firehoseEncryptionKeyArn := terraform.Output(t, terraformOptions, "firehose_server_side_encryption_key_arn")
-
 	assert.Equal(t, kmsKeyArn, firehoseEncryptionKeyArn, "KMS Key ARN and Firehose Server-Side Encryption Key ARN should match")
 
 }
