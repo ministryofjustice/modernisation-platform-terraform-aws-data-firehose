2adfbe8398f23edb87778d3687e6d0914719d376
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-04 10:08:42 +0100

    added a lot of checkov skips for ephemeral unit test resources

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -6,12 +6,21 @@ module "test" {
 }
 
 resource "aws_s3_bucket" "test" {
+  #checkov:skip=CKV_AWS_18
+  #checkov:skip=CKV_AWS_21
+  #checkov:skip=CKV_AWS_144
+  #checkov:skip=CKV_AWS_145
+  #checkov:skip=CKV2_AWS_6
+  #checkov:skip=CKV2_AWS_68
   bucket_prefix = "test"
   force_destroy = true
   tags          = local.tags
 }
 
 resource "aws_cloudwatch_log_group" "test" {
+  #checkov:skip=CKV_AWS_66
+  #checkov:skip=CKV_AWS_158
+  #checkov:skip=CKV_AWS_338
   name_prefix = "test"
   tags        = local.tags
 }
